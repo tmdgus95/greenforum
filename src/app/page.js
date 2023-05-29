f49bd1aef3c32e698d1207c0edac85a48d254398
tmdgus95 <8571053@naver.com>
2023-05-29 16:45:33 +0900

    몽고DB와 연동

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
-export default function Home() {
+import { connectDB } from '@/util/database';
+
+export default async function Home() {
+  let db = (await connectDB).db('forum');
+  let result = await db.collection('post').find().toArray();
+  console.log(result);
+  console.log(1);
   return <main></main>;
 }
