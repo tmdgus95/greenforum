b33f4abc32ab6ca0d4b08d2ed394ee2e589f8c36
tmdgus95 <8571053@naver.com>
2023-05-31 04:12:51 +0900

    삭제기능 구현

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -4,7 +4,10 @@ import { connectDB } from '@/util/database';
 export default async function List() {
   let db = (await connectDB).db('forum');
   let posts = await db.collection('post').find().toArray();
-
+  posts = posts.map((post) => {
+    post._id = post._id.toString();
+    return { ...post, _id: post._id.toString() };
+  });
   return (
     <div className='bg-slate-50 p-3'>
       {posts.map((post, i) => (
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,23 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function PostCard({ post: { title, content, _id } }) {
+  const [hidden, setHidden] = useState(false);
+  const handleDelete = () => {
+    fetch('/api/post/delete', {
+      method: 'POST',
+      body: _id,
+    }).then(() => setHidden(true));
+  };
   return (
-    <div className='bg-white rounded-xl p-5 mb-2'>
+    <div className={`bg-white rounded-xl p-5 mb-2 ${hidden && 'hidden'}`}>
       <Link href={`/detail/${_id}`}>
         <h4 className='text-xl font-extrabold m-0'>{title}</h4>
       </Link>
       <Link href={`/edit/${_id}`}>🪄</Link>
+      <span onClick={handleDelete}>🗑️</span>
       <p className='text-gray-600 mx-0 my-5'>{content}</p>
     </div>
   );
