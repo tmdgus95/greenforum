469f08920b33e4150796825d144d15935e67c185
tmdgus95 <8571053@naver.com>
2023-06-01 15:58:49 +0900

    글 작성기능 수정
    글 작성시 작성자 이메일 등록

--- src/pages/api/post/new.js
+++ src/pages/api/post/new.js
@@ -1,6 +1,12 @@
 import { connectDB } from '@/util/database';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(req, res) {
+  let session = await getServerSession(req, res, authOptions);
+  if (session) {
+    req.body.author = session.user.email;
+  }
   if (req.method == 'POST') {
     if (req.body.title == '') {
       return res.status(500).json('제목이 비어있습니다.');
