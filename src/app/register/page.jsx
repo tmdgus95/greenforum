18f9923dc0cac5245ed07ac5dc798bb38abe4c6a
tmdgus95 <8571053@naver.com>
2023-06-03 21:59:29 +0900

    회원가입 기능 수정

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,11 +1,69 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Register() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('nomal');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (name.trim() === '' || email.trim() === '' || password.trim() === '') {
+      alert('빈칸으로 입력한 란이 있습니다.');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, password, role }),
+      });
+
+      if (response.ok) {
+        alert('회원가입이 완료되었습니다.');
+        router.push('/');
+      } else {
+        const data = await response.json();
+        alert(data.error);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div>
-      <form method='POST' action='/api/auth/signup'>
-        <input name='name' type='text' placeholder='이름' />
-        <input name='email' type='text' placeholder='이메일' />
-        <input name='password' type='password' placeholder='비번' />
-        <button type='submit'>id/pw 가입요청</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          name='name'
+          type='text'
+          placeholder='이름'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          name='email'
+          type='text'
+          placeholder='이메일'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='비번'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>회원가입</button>
       </form>
     </div>
   );
--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -3,10 +3,17 @@ import bcrypt from 'bcrypt';
 
 export default async function handler(req, res) {
   if (req.method == 'POST') {
-    let hash = await bcrypt.hash(req.body.password, 10);
-    req.body.password = hash;
+    const { email, password } = req.body;
     let db = (await connectDB).db('forum');
-    let post = await db.collection('user_cred').insertOne(req.body);
+    const existingUser = await db.collection('user_cred').findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ error: '등록된 이메일입니다.' });
+      return;
+    }
+    let hash = await bcrypt.hash(password, 10);
+    req.body.password = hash;
+    let user = await db.collection('user_cred').insertOne(req.body);
+
     res.redirect(302, '/');
   }
 }
