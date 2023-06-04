67fb539b36bdd3e2abd55cf80e9e3dd5a07e8129
tmdgus95 <8571053@naver.com>
2023-06-04 18:00:49 +0900

    네브바 디지인

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,42 @@
 import Link from 'next/link';
 import { signIn, signOut } from 'next-auth/react';
 import UserProfile from './UserProfile';
+import Image from 'next/image';
 
 export default function Navbar({ session }) {
   return (
     <div className='flex items-center bg-white p-5'>
-      <Link className='mr-3 font-extrabold' href={'/'}>
-        Greenforum
-      </Link>
-      <Link className='mr-3' href={'/list'}>
+      <div className='flex items-center gap-2 duration-300 hover:scale-110'>
+        <Image src={'/images/logo.png'} width={40} height={40} />
+        <Link className='mr-3 font-extrabold' href={'/'}>
+          Greenforum
+        </Link>
+      </div>
+
+      <Link className='mr-3 duration-300 hover:scale-110' href={'/list'}>
         List
       </Link>
-      <Link className='mr-3' href={'/write'}>
+      <Link className='mr-3 duration-300 hover:scale-110' href={'/write'}>
         Write
       </Link>
       {session == null ? (
         <>
-          <Link className='mr-3' href={'/register'}>
+          <Link
+            className='mr-3 duration-300 hover:scale-110'
+            href={'/register'}
+          >
             Register
           </Link>
-          <button onClick={signIn}>Login</button>
+          <button onClick={signIn} className='duration-300 hover:scale-110'>
+            Login
+          </button>
         </>
       ) : (
-        <div className='flex gap-4'>
+        <div className='flex gap-4 '>
           <UserProfile session={session} />
-          <button onClick={signOut}>Logout</button>
+          <button onClick={signOut} className='duration-300 hover:scale-110'>
+            Logout
+          </button>
         </div>
       )}
     </div>
