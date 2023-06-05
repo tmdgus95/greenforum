45b3447b6f5e52249bd6d68fac5f5dda413f8cd3
tmdgus95 <8571053@naver.com>
2023-06-05 17:09:33 +0900

    디테일 페이지 수정
    이미지 추가

--- src/app/detail/[postid]/page.jsx
+++ src/app/detail/[postid]/page.jsx
@@ -5,12 +5,13 @@ import { ObjectId } from 'mongodb';
 export default async function Detail({ params: { postid } }) {
   let db = (await connectDB).db('forum');
   let post = await db.collection('post').findOne({ _id: new ObjectId(postid) });
-  const { title, content } = post;
+  const { title, content, imgurl } = post;
   return (
     <section>
       <h4>상세 페이지</h4>
       <h4>{title}</h4>
       <p>{content}</p>
+      <img src={imgurl} alt={title} width={500} height={500} />
       <Comments _id={postid} />
     </section>
   );
